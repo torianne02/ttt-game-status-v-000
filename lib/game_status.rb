b7de344a193a711b4bba00a67840f67519240cf9
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-20 19:11:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -68,4 +68,9 @@ def over?(board)
 end
 
 def winner(board)
+  if WIN_COMBINATIONS(board)[0] == "X" && WIN_COMBINATIONS(board)[1] == "X" && WIN_COMBINATIONS(board)[2] == "X"
+    return "X"
+  else
+    return "O"
+  end
 end
